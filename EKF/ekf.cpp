7449ce362c2df613c82014b0ef3e2a3d4320691a
Paul Riseborough <p_riseborough@live.com.au>
2016-02-23 17:53:24 +1100

    EKF: Perform initial alignment using down sampled IMU data

--- EKF/ekf.cpp
+++ EKF/ekf.cpp
@@ -136,6 +136,9 @@ bool Ekf::init(uint64_t timestamp)
 
 bool Ekf::update()
 {
+
+	// Only run the filter if IMU data in the buffer has been updated
+	if (_imu_updated) {
 	if (!_filter_initialised) {
 		_filter_initialised = initialiseFilter();
 
@@ -144,8 +147,6 @@ bool Ekf::update()
 		}
 	}
 
-	// Only run the filter if IMU data in the buffer has been updated
-	if (_imu_updated) {
 		// perform state and covariance prediction
 		predictState();
 		predictCovariance();
@@ -227,7 +228,8 @@ bool Ekf::initialiseFilter(void)
 	// Keep accumulating measurements until we have a minimum of 10 samples for the baro and magnetoemter
 
 	// Sum the IMU delta angle measurements
-	_delVel_sum += _imu_down_sampled.delta_vel;
+	imuSample imu_init = _imu_buffer.get_newest();
+	_delVel_sum += imu_init.delta_vel;
 
 	// Sum the magnetometer measurements
 	magSample mag_init = _mag_buffer.get_newest();
