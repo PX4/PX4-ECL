b343ca3bbd0e13812c62b75ca971dd188c97f449
devbharat <bharat.tak8@gmail.com>
2016-04-19 13:51:50 +0200

    Added compensation for VI sensor offset. Check sign.

--- EKF/common.h
+++ EKF/common.h
@@ -249,6 +249,7 @@ struct parameters {
 	Vector3f gps_pos_body;	// xyz position of the GPS antenna in body frame (m)
 	Vector3f rng_pos_body;	// xyz position of range sensor in body frame (m)
 	Vector3f flow_pos_body;	// xyz position of range sensor focal point in body frame (m)
+	Vector3f visn_pos_body;	// xyz position of VI-sensor focal point in body frame (m)
 
 	// Initialize parameter values.  Initialization must be accomplished in the constructor to allow C99 compiler compatibility.
 	parameters()
@@ -330,6 +331,7 @@ struct parameters {
 		gps_pos_body = {};
 		rng_pos_body = {};
 		flow_pos_body = {};
+		visn_pos_body = {};
 	}
 };
 
