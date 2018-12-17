106bf0b2446807a8dbf923fd2ca6d3929060eeac
allu94 <alvar.martti@gmail.com>
2018-12-17 18:45:35 +0200

    updated underspeed criteria

--- tecs/tecs.cpp
+++ tecs/tecs.cpp
@@ -272,7 +272,7 @@ void TECS::_detect_underspeed()
 		return;
 	}
 
-        if (_tas_state < _TAS_min || ((_STE_error > 0) && _underspeed_detected)) {
+        if (_tas_state < _TAS_min * 0.9f || ((_vert_pos_state < _hgt_setpoint_adj) && _underspeed_detected)) {
 
 		_underspeed_detected = true;
 
