536b533846e7046049997fef03bc3875b5ab3e17
kamilritz <kritz@ethz.ch>
2019-12-16 13:54:25 +0100

    Add first fusion logic tests

--- test/sensor_simulator/sensor_simulator.cpp
+++ test/sensor_simulator/sensor_simulator.cpp
@@ -6,7 +6,9 @@ _ekf{ekf},
 _imu{ekf},
 _mag{ekf},
 _baro{ekf},
-_gps{ekf}
+_gps{ekf},
+_flow{ekf},
+_rng{ekf}
 {
 
 	// set default sensor rate in Hz
@@ -14,6 +16,8 @@ _gps{ekf}
 	_mag.setRate(80);
 	_baro.setRate(80);
 	_gps.setRate(5);
+	_flow.setRate(50);
+	_rng.setRate(30);
 
 	// set default sensor data
 	_imu.setData(Vector3f{0.0f,0.0f,-CONSTANTS_ONE_G},
@@ -21,11 +25,15 @@ _gps{ekf}
 	_mag.setData(Vector3f{0.2f, 0.0f, 0.4f});
 	_baro.setData(122.2f);
 	_gps.setData(getDefaultGpsData());
+	_flow.setData(_flow.dataAtRest());
+	_rng.setData(0.2f, 100);
+
 
 	// start default sensor
 	_imu.start();
 	_mag.start();
 	_baro.start();
+
 }
 
 SensorSimulator::~SensorSimulator()
@@ -74,6 +82,8 @@ void SensorSimulator::run_microseconds(uint32_t duration)
 		_mag.update(_time);
 		_baro.update(_time);
 		_gps.update(_time);
+		_flow.update(_time);
+		_rng.update(_time);
 
 		_ekf->update();
 	}
