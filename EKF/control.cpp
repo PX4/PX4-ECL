9dd42dadc7ad61e0190d35f941b1f6f7090543ba
Paul Riseborough <p_riseborough@live.com.au>
2020-02-19 09:45:13 +1100

    EKF: Rework EKF-GSF reset timers
    Changes arising from peer review.

--- EKF/control.cpp
+++ EKF/control.cpp
@@ -662,17 +662,19 @@ void Ekf::controlGpsFusion()
 			// This special case reset can also be requested externally.
 			// The minimum time interval between resets to the EKF-GSF estimate must be limited to
 			// allow the EKF-GSF time to improve its estimate if the first reset was not successful.
-			const bool stopped_following_gps_velocity = (_time_last_imu - _time_last_hor_vel_fuse) > _params.EKFGSF_reset_delay &&
+			const bool stopped_following_gps_velocity = isTimedOut(_time_last_hor_vel_fuse, _params.EKFGSF_reset_delay) &&
 							   (_time_last_hor_vel_fuse > _time_last_on_ground_us);
 			if (!_control_status.flags.in_air) {
 				_time_last_on_ground_us = _time_last_imu;
 			}
-			const bool recent_takeoff = (_control_status.flags.in_air && (_imu_sample_delayed.time_us - _time_last_on_ground_us) < 30000000);
-			const bool reset_yaw_to_EKFGSF = (do_reset || _do_emergency_yaw_reset || stopped_following_gps_velocity) && recent_takeoff && ((_imu_sample_delayed.time_us - _emergency_yaw_reset_time) > 5000000);
+			const bool recent_takeoff = _control_status.flags.in_air && !isTimedOut(_time_last_on_ground_us, 30000000);
+			const bool reset_yaw_to_EKFGSF = (do_reset || _do_emergency_yaw_reset || stopped_following_gps_velocity) &&
+								recent_takeoff &&
+								isTimedOut(_emergency_yaw_reset_time, 5000000);
 
-			if (reset_yaw_to_EKFGSF && (_imu_sample_delayed.time_us - _emergency_yaw_reset_time) > 5000000) {
+			if (reset_yaw_to_EKFGSF) {
 				if (resetYawToEKFGSF()) {
-					_emergency_yaw_reset_time = _imu_sample_delayed.time_us;
+					_emergency_yaw_reset_time = _time_last_imu;
 					_do_emergency_yaw_reset = false;
 
 					// Reset the timeout counters
